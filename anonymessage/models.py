9450748df83bfc5756dfa2676e2e4bfea16c9930
kosytedy <kosytedy@yahoo.com>
2019-12-02 06:39:35 -0600

    Created models, migrated and register at admin

--- anonymessage/models.py
+++ anonymessage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    provider = models.CharField(max_length=15)
+    provider_id = models.CharField(max_length=200)
+    def __str__(self):
+        return self.provider_id
+
+
+class Message(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField(max_length=500)
+    def __str__(self):
+        return self.message
